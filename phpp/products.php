66023f103b39ca9c93ddee40111cb8e80f4c2c8b
Dorian <doriangueguen@orange.fr>
2018-04-12 17:06:12 +0200

    php modifs

--- phpp/products.php
+++ phpp/products.php
@@ -1,83 +1,54 @@
 <?php
-session_start();
 include_once 'includes/header.php';
+include_once 'includes/panier.php';
+require_once 'connexion.php';
+session_start();
+$requete = "SELECT
+  `id`, 
+  `price`, 
+  `name`,
+  `title`
+FROM 
+  `cart`
+  WHERE
+  `id`
+  BETWEEN  15 AND 17
+;";
+$stmt = $conn->prepare($requete);
+$stmt->execute();
 ?>
 
-<section class="products">
+    <section class="products">
+<?php while (false !== $row = $stmt -> fetch(PDO::FETCH_ASSOC)):?>
     <a class="productsLink" href="add_Comment.php">Personnaliser ici votre cigarette.</a>
     <section class="productsItem gecko">
-        <div class="productsItemImg">
-            <p class="productsItemTitle">Gecko</p>
-            <img class="productsImg" src="assets/imgs/gecko.jpg" alt="yeh">
-        </div>
-        <div class="productsItemDesc">
-            <p class="productsItemPrice">69 €</p>
-            <div class="productsItemText-all">
-                <p class="productsItemText-high">La cigarette la plus apprécié selon nos utilisateurs.</p>
-                <div class="productsImgContainer">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/starwhite.png" alt="">
-                </div>
-                <p class="productsItemText-low">Gecko est un concentré de performance et de design concentré dans un micro setup de moins de 10 cm.</p>
+    <div class="productsItemImg">
+        <p class="productsItemTitle"><?= $row['name'] ?></p>
+        <img class="productsImg" src="assets/imgs/gecko.jpg" alt="yeh">
+    </div>
+    <div class="productsItemDesc">
+        <p class="productsItemPrice">69 €</p>
+        <div class="productsItemText-all">
+            <p class="productsItemText-high">La cigarette la plus apprécié selon nos utilisateurs.</p>
+            <div class="productsImgContainer">
+                <img class="productStars" src="assets/imgs/star.png" alt="">
+                <img class="productStars" src="assets/imgs/star.png" alt="">
+                <img class="productStars" src="assets/imgs/star.png" alt="">
+                <img class="productStars" src="assets/imgs/star.png" alt="">
+                <img class="productStars" src="assets/imgs/starwhite.png" alt="">
             </div>
-            <div class="productsItemButton"><button class="productsItemAdd" type="button" name="button">Ajouter aux panier</button></div>
+            <p class="productsItemText-low">Gecko est un concentré de performance et de design concentré dans un micro setup de moins de 10 cm.</p>
         </div>
-</section>
-
-    <section class="productsItem gecko">
-        <div class="productsItemImg ImgVaporesso">
-            <p class="productsItemTitle TitleVaporesso ">Vaporesso</p>
-            <img class="productsImg" src="assets/imgs/vaporesso.png" alt="yeh">
+        <div class="productsItemButton">
+            <form method="post" action="add_panier.php">
+                <button class="productsItemAdd" type="submit" name="button">Ajouter aux panier</button>
+            </form>
         </div>
-        <div class="productsItemDesc DescVaporesso">
-            <p class="productsItemPrice">99 €</p>
-            <div class="productsItemText-all">
-                <p class="productsItemText-high TextVaporesso">500+ utilisateurs ont acheter cette cigarette</p>
-                <div class="productsImgContainer">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                    <img class="productStars" src="assets/imgs/star.png" alt="">
-                </div>
-                <p class="productsItemText-low TextVaporesso">Le Kit Attitude s'adresse ainsi aux vapoteurs à la recherche d'un appareil offrant un niveau de contrôle supérieur sans avoir à passer par de la programmation informatique.</p>
-            </div>
-            <div class="productsItemButton"><button class="productsItemAdd BtnVaporesso" type="button" name="button">Ajouter aux panier</button></div>
-        </div>
+    </div>
+<?php endwhile; ?>
     </section>
 
 
-        <section class="productsItem gecko">
-            <div class="productsItemImg">
-                <p class="productsItemTitle">Passy</p>
-                <img class="productsImg" src="assets/imgs/passy.jpg" alt="yeh">
-            </div>
-            <div class="productsItemDesc">
-                <p class="productsItemPrice">149 €</p>
-                <div class="productsItemText-all">
-                    <p class="productsItemText-high">Des stars comme Rihanna on acheté cette cigarette.</p>
-                    <div class="productsImgContainer">
-                        <img class="productStars" src="assets/imgs/star.png" alt="">
-                        <img class="productStars" src="assets/imgs/star.png" alt="">
-                        <img class="productStars" src="assets/imgs/star.png" alt="">
-                        <img class="productStars" src="assets/imgs/star.png" alt="">
-                        <img class="productStars" src="assets/imgs/starwhite.png" alt="">
-                    </div>
-                    <p class="productsItemText-low">La nouvelle Passy avec l'atomiseur Mini Atlas V2 embarqué procure une vape généreuse et conséquente.Le meilleur du tout-en-un au creux de la main.</p>
-                </div>
-                <div class="productsItemButton"><button class="productsItemAdd" type="button" name="button">Ajouter aux panier</button></div>
-            </div>
-        </section>
-</section>
 <?php
-    include_once 'includes/footer.php';
+include 'includes/footer.php';
 ?>
-
-<script src="js/loginModal.js"></script>
-<script src="js/bagMarketModal.js"></script>
-
-</body>
-</html>
